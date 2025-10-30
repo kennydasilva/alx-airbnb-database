1b6b49e88f9b9da8ce173c5e2633a9b07b3f80b0
Kenny Dasilva <kennydasilva542@gmail.com>
2025-10-30 14:16:34 +0200

    aggregations_and_window_functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,6 +9,7 @@ FROM users u
 LEFT JOIN Booking b ON u.user_id = b.user_id
 GROUP BY u.user_id
 ORDER BY total_bookings DESC;
+
 -- ==========================================================
 --  WINDOW FUNCTION: Rank properties by total bookings
 -- ==========================================================
@@ -16,8 +17,8 @@ SELECT
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank_position
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank_position,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_num_position
 FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name;
-
